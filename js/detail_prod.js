067c2eb8c24d67f6c66a60f91b1cea1049ec4115
MaiThanhPhuc <maithanhphuc7721@gmail.com>
2021-11-20 11:21:18 +0700

    update more picture

--- js/detail_prod.js
+++ js/detail_prod.js
@@ -21,93 +21,65 @@ $("#booking-box-guests").click(function (e) {
 $("#booking-box-guests").click(function (e) {
   $(".booking-box-guests-number").addClass("active");
 });
+// review score
 
-// btn add sub
-$(".booking-box-guests__btn-adult-add").click(function (e) {
-  var temp = parseInt(
-    document.getElementById("booking-box-guests__count--adult").innerHTML
-  );
-  var quantity = parseInt(
-    document.getElementById("booking-box-guests--input").value
-  );
+// score bar
 
-  if (temp < 5) {
-    temp += 1;
-    quantity += 1;
-    document.getElementById("booking-box-guests__count--adult").innerHTML =
-      temp;
-    document.getElementById("booking-box-guests--input").value =
-      quantity + " Khách";
+function init() {
+  var list_score = document.getElementsByClassName(
+    "room-review-comment-block__score-total"
+  );
+  var len = document.getElementsByClassName(
+    "room-review-comment-block__score-total"
+  ).length;
+  var container_score = [];
+  var avg_score = 0;
+  for (var i = 0; i < len; i++) {
+    var temp = parseFloat(list_score[i].innerHTML);
+    container_score.push(temp);
+    avg_score += temp;
   }
-});
-$(".booking-box-guests__btn-adult-sub").click(function (e) {
-  var quantity = parseInt(
-    document.getElementById("booking-box-guests--input").value
-  );
-  var temp = parseInt(
-    document.getElementById("booking-box-guests__count--adult").innerHTML
-  );
-  if (temp > 1) {
-    temp -= 1;
-    quantity -= 1;
-    document.getElementById("booking-box-guests__count--adult").innerHTML =
-      temp;
-    document.getElementById("booking-box-guests--input").value =
-      quantity + " Khách";
-  }
-});
-$(".booking-box-guests__btn-child-add").click(function (e) {
-  var temp = parseInt(
-    document.getElementById("booking-box-guests__count--child").innerHTML
-  );
-  var quantity = parseInt(
-    document.getElementById("booking-box-guests--input").value
-  );
-  if (temp < 5) {
-    temp += 1;
-    quantity += 1;
-    document.getElementById("booking-box-guests__count--child").innerHTML =
-      temp;
-    document.querySelector(".booking-box-guests-number-warning").className +=
-      " active";
-    document.querySelector(".booking-box-guests-age-child").className +=
-      " active";
-    document.getElementById("booking-box-guests--input").value =
-      quantity + " Khách";
-  }
-});
-$(".booking-box-guests__btn-child-sub").click(function (e) {
-  var temp = parseInt(
-    document.getElementById("booking-box-guests__count--child").innerHTML
-  );
-  var quantity = parseInt(
-    document.getElementById("booking-box-guests--input").value
-  );
-  if (temp > 0) {
-    temp -= 1;
-    quantity -= 1;
-    document.getElementById("booking-box-guests__count--child").innerHTML =
-      temp;
-    document.querySelector(".booking-box-guests-number-warning").className =
-      document
-        .querySelector(".booking-box-guests-number-warning")
-        .className.replace(" active", "");
-    document.querySelector(".booking-box-guests-age-child").className = document
-      .querySelector(".booking-box-guests-age-child")
-      .className.replace(" active", "");
-    document.getElementById("booking-box-guests--input").value =
-      quantity + " Khách";
-  }
-});
+  document.getElementById("avg_score").innerHTML = (avg_score / len).toFixed(1);
 
-// end
+  var container_1 = 0,
+    container_2 = 0,
+    container_3 = 0,
+    container_4 = 0,
+    container_5 = 0;
+  container_score.forEach((x) => {
+    if (x < 5 && x >= 4) {
+      container_1 += 1;
+    } else if (x < 4 && x >= 3) {
+      container_2 += 1;
+    } else if (x < 3 && x >= 2) {
+      container_3 += 1;
+    } else if (x < 2 && x >= 1) {
+      container_4 += 1;
+    } else {
+      container_5 += 1;
+    }
+  });
+  document.getElementById("room-review-average-score-bar__1").style.width = `${
+    (container_1 * 100) / len
+  }%`;
+  document.getElementById("room-review-average-score-bar__2").style.width = `${
+    (container_2 * 100) / len
+  }%`;
+  document.getElementById("room-review-average-score-bar__3").style.width = `${
+    (container_3 * 100) / len
+  }%`;
+  document.getElementById("room-review-average-score-bar__4").style.width = `${
+    (container_4 * 100) / len
+  }%`;
+  document.getElementById("room-review-average-score-bar__5").style.width = `${
+    (container_5 * 100) / len
+  }%`;
+}
 
-// total price
-var room_price = parseFloat(
-  document.querySelector(".booking-box-heading__price h3").innerHTML
-);
-console.log(room_price);
-
-var y = 1000;
-y = y.toLocaleString("en-US", {style: "currency", currency: "USD"});
-console.log(y);
+init();
+$(".image-container").click(function (e) {
+  document.getElementById("more_image").style.display = "block";
+});
+$(".modal-body-btn-back").click(function (e) {
+  document.getElementById("more_image").style.display = "none";
+});
